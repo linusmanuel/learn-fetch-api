3329df95a890942245152ab3b499973e970391fb
Joao Lino <linusmannuel@gmail.com>
2023-06-06 22:24:17 +0100

    using fetch API

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const inputCEP = document.querySelector('#cep');
 const searchCEP = document.querySelector('#btnPesquisar');
-const cepValue = inputCEP.value;
+const street = document.querySelector('#rua');
+const complement = document.querySelector('#complemento');
+const neighborhood = document.querySelector('#bairro');
+const city = document.querySelector('#cidade');
+const state = document.querySelector('#estado');
+
 searchCEP.addEventListener('click', (event) => {
 	event.preventDefault();
+	const cepValue = inputCEP.value;
 	getData(cepValue);
 });
 
 function getData(cep) {
 	const url = `https://viacep.com.br/ws/${cep}/json/`;
-	return console.log(url);
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			street.value = data.logradouro;
+			complement.value = data.complemento;
+			neighborhood.value = data.bairro;
+			city.value = data.localidade;
+			state.value = data.uf;
+			console.log(data);
+		});
 }
